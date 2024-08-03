e43fad04d2f61d8783fe7c6f0f182023c9b19bc6
HokArt <kokouhoka@gmail.com>
2024-08-03 14:24:39 +0000

    commit seven

--- src/app/components/Meteo/Meteo.jsx
+++ src/app/components/Meteo/Meteo.jsx
@@ -57,7 +57,7 @@ const Meteo = () => {
               alt='Icône météo'
             />
           </span>
-          <form onSubmit={handleSearch} className='flex max-[500px]:ml-[15vw] max-[375px]:ml-5 max-[345px]:ml-1'>
+          <form onSubmit={handleSearch} className='flex max-[500px]:ml-[15vw] max-[375px]:ml-5 max-[345px]:ml-[5vw]'>
             <input
               type='text'
               value={input}
